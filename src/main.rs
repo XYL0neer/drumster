c67e63138c3953ea00692c031302c6dd2cba38d6
Adrian Beilharz <Adrian.Beilharz@EXXETA.com>
2022-12-14 15:18:15 +0100

    parse csv

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{io::BufReader, thread, time::Duration};
 use clap::Parser;
 use rodio::Sink;
 
+pub mod parse_drum_machine;
+
 #[derive(Parser)]
 #[clap(author, version, about)]
 struct CLI {
@@ -18,27 +20,30 @@ fn main() {
 
     let sample = std::fs::read_to_string(sample).expect("Should have been able to read the file");
 
-    let mut instrument;
+    let play_machine = parse_drum_machine::parse_csv(&sample);
 
-    let mut threads = vec![];
+    dbg!(&play_machine);
 
-    for x in sample.split(";") {
-        println!("{}", x);
-        if x == "kick" {
-            instrument = x;
-        } else {
-            threads.push(thread::spawn(|| {
-                let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-
-                let sink = rodio::Sink::try_new(&handle).unwrap();
-                play_sound(&sink);
-                sink.sleep_until_end();
-            }));
-            let duration = x.parse::<u64>().unwrap();
-            thread::sleep(Duration::new(duration, 0));
-        }
-    }
-    threads.into_iter().for_each(|t| t.join().unwrap());
+    // concept for playing a sound
+    // let mut threads = vec![];
+    //
+    // for x in sample.split(";") {
+    //     println!("{}", x);
+    //     if x == "kick" {
+    //         instrument = x;
+    //     } else {
+    //         threads.push(thread::spawn(|| {
+    //             let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    //
+    //             let sink = rodio::Sink::try_new(&handle).unwrap();
+    //             play_sound(&sink);
+    //             sink.sleep_until_end();
+    //         }));
+    //         let duration = x.parse::<u64>().unwrap();
+    //         thread::sleep(Duration::new(duration, 0));
+    //     }
+    // }
+    // threads.into_iter().for_each(|t| t.join().unwrap());
 }
 
 fn play_sound(sink: &Sink) {
---
+++ src/parse_drum_machine.rs
@@ -0,0 +1,70 @@
+use std::collections::VecDeque;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct DrumMachine {
+    beats: u8,
+    base: u8,
+    bpm: u32,
+    resolution: u32,
+    tracks: Vec<Track>,
+}
+
+#[derive(Debug)]
+pub struct Track {
+    instrument: Instrument,
+    triggers: Vec<u32>,
+}
+
+#[derive(Debug)]
+enum Instrument {
+    Kick,
+    Snare,
+    HiHat,
+}
+
+pub fn parse_csv(csv_content: &String) -> DrumMachine {
+    let mut lines = csv_content.lines().collect::<VecDeque<&str>>();
+
+    let config_line = lines.pop_front().unwrap();
+
+    let (beats, config_line) = next_element(config_line);
+    let (base, config_line) = next_element(config_line);
+    let (bpm, config_line) = next_element(config_line);
+    let (resolution, config_line) = next_element(config_line);
+
+    let instruments: Vec<Track> = Vec::new();
+
+    let tracks: Vec<Track> = config_line.split(';').map(|(inst)| {
+        let instrument = match inst {
+            "Kick" => Ok(Instrument::Kick),
+            "Snare" => Ok(Instrument::Snare),
+            "HiHat" => Ok(Instrument::HiHat),
+            _ => Err(())
+        };
+        let line = lines.pop_front().unwrap();
+        create_track(instrument.unwrap(), line)
+    }).collect();
+
+    let drum_machine = DrumMachine {
+        beats: beats.parse().unwrap(),
+        base: base.parse().unwrap(),
+        bpm: bpm.parse().unwrap(),
+        resolution: resolution.parse().unwrap(),
+        tracks,
+    };
+
+    drum_machine
+}
+
+fn next_element(line: &str) -> (&str, &str) {
+    line.split_once(';').unwrap()
+}
+
+fn create_track(inst: Instrument, line: &str) -> Track {
+    let line: Vec<u32> = line.split(';').map(|x| x.parse::<u32>().unwrap()).collect();
+    Track {
+        instrument: inst,
+        triggers: line
+    }
+}
