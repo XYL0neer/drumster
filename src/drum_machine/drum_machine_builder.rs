30e30e9de7bfc0e9eefac1f425d2f68c8174894e
XYL0neer <abeilharz9@gmail.com>
2023-02-23 15:45:46 +0100

    prepare strategy pattern

--- src/drum_machine/drum_machine_builder.rs
+++ src/drum_machine/drum_machine_builder.rs
@@ -1,87 +1,82 @@
-use std::io::{Error, ErrorKind};
+use std::io::{Error};
 
 use super::model::{DrumMachine, Instrument, Track};
 
-pub enum TriggerPattern {
-    OffBeat,
-    OnBeat,
-    BackBeat,
-    Shuffle,
+pub trait TriggerPattern {
+    fn to_triggers(&self, beats: u8, base: u8, resolution: u32) -> Vec<u32>;
 }
 
-impl TriggerPattern {
-    pub fn to_triggers(self, strokes: u32, beats: u8) -> Vec<u32> {
+// OffBeat, // Hit between to beats
+  //  OnBeat, // Hit on every beat
+   // BackBeat, // Hit on 2 and 4
+    //Shuffle, // Hit on Beat and 2 resolution lower before the beat
+
+struct OnBeat;
+impl TriggerPattern for OnBeat {
+    fn to_triggers(&self, beats: u8, base: u8, resolution: u32) -> Vec<u32> {
+        let stroke_per_beat = resolution as f64 / base as f64;
+        println!("{} strokes per beat", stroke_per_beat);
+        let stroke_per_tact = stroke_per_beat as u32 * beats as u32;
         vec![]
     }
 }
 
 #[derive(Default)]
 pub struct DrumMachineBuilder {
-    pub bpm: Option<u32>,
-    pub beats: Option<u8>,
-    pub base: Option<u8>,
-    pub resolution: Option<u32>,
+    pub bpm: u32,
+    pub beats: u8,
+    pub base: u8,
+    pub resolution: u32,
     pub tracks: Vec<Track>,
 }
 
 impl DrumMachineBuilder {
     pub fn new() -> Self {
         Self {
+            bpm: 80,
+            beats: 4,
+            base: 4,
+            resolution: 16,
             tracks: vec![],
-            ..Default::default()
         }
     }
 
     pub fn set_bpm(mut self, bpm: u32) -> Self {
-        self.bpm = Some(bpm);
+        self.bpm = bpm;
         self
     }
 
     pub fn set_time_signature(mut self, beats: u8, base: u8) -> Self {
-        self.beats = Some(beats);
-        self.base = Some(base);
+        self.beats = beats;
+        self.base = base;
         self
     }
 
     pub fn set_resolution(mut self, resolution: u32) -> Self {
-        self.resolution = Some(resolution);
+        self.resolution = resolution;
         self
     }
 
-    pub fn add_track(
+    pub fn add_track<R: TriggerPattern>(
         mut self,
         instrument: Instrument,
-        pattern: TriggerPattern,
+        pattern: R,
     ) -> Result<Self, Error> {
-        if let None = self.beats {
-            return Err(Error::new(
-                ErrorKind::Other,
-                "Set time signature before adding a track",
-            ));
-        }
-        if let None = self.resolution {
-            return Err(Error::new(
-                ErrorKind::Other,
-                "Set resolution before adding a Track",
-            ));
-        }
-        let stroke_per_beat = self.resolution.unwrap() as f64 / self.base.unwrap() as f64;
-        println!("{} strokes per beat", stroke_per_beat);
-        let stroke_per_tact = stroke_per_beat as u32 * self.beats.unwrap() as u32;
-        let triggers = pattern.to_triggers(stroke_per_tact, self.beats.unwrap());
+
+        let triggers = pattern.to_triggers(self.beats, self.base, self.resolution);
         self.tracks.push(Track {
-            instrument: instrument,
-            triggers: triggers,
+            instrument,
+            triggers,
         });
         Ok(self)
     }
 
     pub fn build(self) -> DrumMachine {
         DrumMachine {
-            bpm: self.bpm.unwrap(),
-            beats: self.beats.unwrap(),
-            base: self.base.unwrap(),
-            resolution: self.resolution.unwrap(),
+            bpm: self.bpm,
+            beats: self.beats,
+            base: self.base,
+            resolution: self.resolution,
             tracks: self.tracks,
         }
     }
